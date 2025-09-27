cfb81f95739e1a4a2cd5a2df5618ee8165d6dc10
mwamure254 <mwamuredkwicha@gmail.com>
2025-09-27 15:24:32 +0300

    first farm

---
+++ src/test/java/com/mfano/farmestate/FarmestateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfano.farmestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmestateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
